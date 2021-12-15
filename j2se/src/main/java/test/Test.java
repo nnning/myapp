45bb22d36d1bd22e8775d20a07c261482f4db283
Ning Zhang <ningwazi@gmail.com>
2021-12-15 17:27:29 +0100

    commit

--- j2se/src/main/java/test/Test.java
+++ j2se/src/main/java/test/Test.java
@@ -1,8 +1,31 @@
 package test;
 
+import java.sql.*;
+
 public class Test {
 
-    public static void main(String[] args) {
-        System.out.println("hello");
+    public static void main(String[] args) throws Exception{
+        String url = "jdbc:postgresql://localhost/postgres";
+        String user = "postgres";
+        String password = "1029";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+            Statement s = conn.createStatement();
+            String sql = "SELECT * FROM weather";
+            ResultSet rs = s.executeQuery(sql);
+            while (rs.next()) {
+                System.out.println(rs.getString(1));
+                System.out.println(rs.getInt(2));
+                System.out.println(rs.getInt(3));
+                System.out.println(rs.getDouble(4));
+                System.out.println(rs.getString(5));
+
+
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
